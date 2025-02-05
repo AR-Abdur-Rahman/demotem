314efb688a0955f669206ad76e50c2438433443c
Abdur Rahman <149816856+AR-Abdur-Rahman@users.noreply.github.com>
2025-02-05 14:56:44 +0600

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing', {
+
+    string: ['Web Designer', 'Web Developer', 'Wordprees Developer', 'Freelanser'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
